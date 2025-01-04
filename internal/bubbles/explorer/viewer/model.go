a1f5d602ef75b7f92110d0ae84fddef35244930b
Bruno Luiz Silva <contact@brunoluiz.net>
2025-01-04 12:43:31 +0000

    chore: remove old special renders for viewer
    They are actually irrelevant, what is more relevant is to filter out data

--- internal/bubbles/explorer/viewer/model.go
+++ internal/bubbles/explorer/viewer/model.go
@@ -2,17 +2,12 @@ package viewer
 
 import (
 	"fmt"
-	"strings"
-	"time"
 
 	"github.com/brunoluiz/crossplane-explorer/internal/bubbles/viewer"
 	"github.com/brunoluiz/crossplane-explorer/internal/xplane"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
-	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
 	"github.com/goccy/go-yaml"
-	"github.com/samber/lo"
-	k8sv1 "k8s.io/api/core/v1"
 )
 
 type Model struct {
@@ -41,53 +36,12 @@ func (m *Model) SetContent(msg ContentInput) {
 		panic(err)
 	}
 
-	hr := "────"
-	if m.viewer.GetWidth() > 4 {
-		hr = strings.Repeat("─", m.viewer.GetWidth()-4)
-	}
-
 	m.viewer.SetContent(viewer.ContentInput{
 		Title:     fmt.Sprintf("%s/%s", msg.Trace.Unstructured.GetKind(), msg.Trace.Unstructured.GetName()),
 		SideTitle: msg.Trace.Unstructured.GetAPIVersion(),
 		Content: m.styles.Main.Render(lipgloss.JoinVertical(
 			lipgloss.Top,
-			m.renderHealth("synced", msg.Trace.GetCondition(xpv1.TypeSynced)),
-			m.renderHealth("ready", msg.Trace.GetCondition(xpv1.TypeReady)),
-			m.renderMetadata(msg.Trace.Unstructured.GetAnnotations()),
-			hr,
 			string(val),
 		)),
 	})
-}
-
-func (m Model) renderHealth(name string, c xpv1.Condition) string {
-	info := []string{}
-	s := m.styles.BadHealth
-	n := lo.Capitalize(name)
-	if c.Status == k8sv1.ConditionTrue {
-		s = m.styles.OkHealth
-	}
-
-	if c.Reason == "" {
-		info = append(info, s.Render(fmt.Sprintf("%s: %s", n, c.Status)))
-	} else {
-		info = append(info, s.Render(fmt.Sprintf("%s: %s (%s)", n, c.Status, c.Reason)))
-	}
-
-	if c.Message != "" {
-		info = append(info, m.styles.Idented.Render(fmt.Sprintf("Message: %s", c.Message)))
-	}
-
-	info = append(info, m.styles.Idented.Render(fmt.Sprintf("Last Transition Time: %s", c.LastTransitionTime.Format(time.RFC822))))
-	return lipgloss.JoinVertical(lipgloss.Top, info...)
-}
-
-func (m Model) renderMetadata(annotations map[string]string) string {
-	info := []string{}
-	info = append(info, m.styles.Metadata.Render("Annotations:"))
-	for k, v := range annotations {
-		info = append(info, m.styles.Idented.Render(fmt.Sprintf("%s: \"%s\"", k, v)))
-	}
-
-	return lipgloss.JoinVertical(lipgloss.Top, info...)
 }
